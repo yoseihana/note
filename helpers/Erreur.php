47e61df5a0104fdf4c0c76469a2352f9cd46a413
Annabelle Buffart <anna.buffart@gmail.com>
2012-06-03 17:08:39 +0200

    gestion erreur+connexion

---
+++ helpers/Erreur.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: annabelle
+ * Date: 3/06/12
+ * Time: 15:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once './controlleur/ErreurController.php';
+
+final class Erreur
+{
+    private static function build($action, array $param = array())
+    {
+        $p = array(
+            'c'=> ErreurController::getName(),
+            'a'=> $action,
+        );
+
+        header('Location: ' . $_SERVER['PHP_SELF'] . '?' . http_build_query(array_merge($p, $param)));
+    }
+
+    public static function erreurLogin()
+    {
+
+        return Erreur::build('erreurLogin');
+    }
+
+    public static function erreurAction()
+    {
+
+        return Erreur::build('erreurAction');
+    }
+
+    public static function erreurController()
+    {
+        return Erreur::build('erreurController');
+    }
+
+    public static function erreurControllerExiste()
+    {
+        return Erreur::build('erreurControllerExiste');
+    }
+
+    public static function erreurDefaut()
+    {
+        return Erreur::build('erreurDefaut');
+    }
+
+    public static function erreurParam()
+    {
+        return Erreur::build('erreurParam');
+    }
+}
--- helpers/Url.php
+++ helpers/Url.php
@@ -1,4 +1,8 @@
 <?php
+
+require_once './controlleur/NoteController.php';
+require_once './controlleur/MembreController.php';
+
 final class Url
 {
     private static function build($controller, $action, array $param = array())
@@ -34,5 +38,15 @@ final class Url
     public static function supprimerNote($id)
     {
         return Url::build(NoteController::getName(), 'supprimer', array('id'=> $id));
+    }
+
+    public static function connexionMembre()
+    {
+        return Url::build(MembreController::getName(), 'connexion');
+    }
+
+    public static function deconnexionMembre()
+    {
+        return Url::build(MembreController::getName(), 'deconnexion');
     }
 }
